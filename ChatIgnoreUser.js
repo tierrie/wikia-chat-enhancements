a95e7cba58c2e9279a2ca6c071f3bb66a95527ad
tierrie <tierrie@code-poets.org>
2014-02-13 00:26:58 -0800

    Beta - Fairly Stable

--- ChatIgnoreUser.js
+++ ChatIgnoreUser.js
@@ -17,23 +17,11 @@ if(wgCanonicalSpecialPageName == 'Chat') {
       $('head').append('<style type="text/css" id="chat_ignored_list">li[data-user="'+response.blockedChatUsers.join('"], li[data-user="')+'"] {display:none;} #Rail li[data-user] {display:list-item !important;}</style>');
     });
 
-
-
-
-
-
-
-
-
-
-
-    // extend the chat options to manage the block list
-
     /**
-     * Displays the options window
+     * Displays the IgnoreList window
      */
     function openIgnoreList() {
-      $.showCustomModal( "Ignore Users", '<form class="WikiaForm"><fieldset><div class="input-group"><label>Block list</label><ul id="ignored_user_list"><li><input type="text" value=""><span class="icon add">&nbsp;</span></li></ul></div></fieldset></form>',
+      $.showCustomModal( "Blocked User List", '<form class="WikiaForm"><fieldset><div class="input-group"><label>Ignored users</label><ul id="ignored_user_list"></ul><ul id="ignore_user_add"><li><input type="text" value="" name="ignore_user_add_input"><a class="icon add">&nbsp;</a></li></ul></div></fieldset></form>',
       {
         id: "chat_ignore_list_dialog",
         width: 450,
@@ -47,7 +35,34 @@ if(wgCanonicalSpecialPageName == 'Chat') {
           }
         ]
       });
-      // enum through the available window fonts
+      
+      // get the list of ignored users and display it
+      $.getJSON('/index.php?action=ajax&rs=ChatAjax&method=getListOfBlockedPrivate', function(response) {
+        for (var i=0;i<response.blockedChatUsers.length;i++) {
+          $('#ignored_user_list').append('<li class="ignored_user" data-blocked-user="' + response.blockedChatUsers[i] + '"><span>' + response.blockedChatUsers[i] + '</span><a class="icon remove">&nbsp;</a></li>');
+        }
+      });
+      
+      // hook the delete button to removing the user from the block list
+      $(document).on('click', '#ignored_user_list a.icon.remove', function () {
+        $(this).parent().slideUp(100, function() {
+          mainRoom.allowPrivate({name: $(this).data("blocked-user")});
+      	  $(this).remove();
+        });
+      });
+      
+      // hook the add button to add an entry to the list
+      $(document).on('click', '#ignore_user_add a.icon.add', function () {
+        if ( $('#ignore_user_add input[type=text]').val().length != 0 ) {
+          mainRoom.blockPrivate({name: $('#ignore_user_add input[type=text]').val()});
+          
+          $('#ignored_user_list').append('<li class="ignored_user" data-blocked-user="' + $('#ignore_user_add input[type=text]').val() + '"><span>' + $('#ignore_user_add input[type=text]').val() + '</span><a class="icon remove">&nbsp;</a></li>');
+          
+          $('#ignored_user_list').find('li:last').hide();
+          $('#ignored_user_list').find('li:last').slideDown(200);
+          $('#ignore_user_add input[type=text]').val('');
+        }
+      });
     }
     
     /**
@@ -59,17 +74,6 @@ if(wgCanonicalSpecialPageName == 'Chat') {
     } // end closeOptions()
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
- 
     // hook onto the function that handles blocking PM so that it also hides main chat messages
     NodeChatController.prototype.blockAllowPrivateAjax = function (name, dir, callback) {
       $.ajax({
@@ -88,12 +92,15 @@ if(wgCanonicalSpecialPageName == 'Chat') {
       });
     }
     
+    /*
+    // add the icon to open ignore list
+    if (!$("#chat-ignore-list-button").length) {
+      //$('#chat-plugin-footer').append('<div id="chat-ignore-list-button"><img height="16" width="16" class="sprite gear" src="data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D" /></div>');
+      $("#chat-ignore-list-button")$('#chat-ignore-list-button').click(openIgnoreList);
+    }*/
     
-    // add a block list icon
-    if (!$('.Rail #chat-ignore-list-button').length) {
-      $('.Rail').append('<div id="chat-ignore-list-button"><img height="16" width="16" class="sprite gear" src="data:image/gif;base64,R0lGODlhAQABAIABAAAAAP///yH5BAEAAAEALAAAAAABAAEAQAICTAEAOw%3D%3D" /></div>');
-      $('.Rail #chat-ignore-button').click(openIgnoreList);
-    }
+    // delegate the hook to #chat-ignore-list-button
+    $('body').on('click', '#chat-ignore-list-button', openIgnoreList);
     
     console.log("LOG: Chat/IgnoreUsers.js loaded");
   })(); // execute the anonymous function for scoping
